1114e82616644074532b8d581292c1e44f3d0025
Cleibson Gomes da Silva <c.gomes.da.silva@avanade.com>
2020-04-07 21:30:05 -0300

    Inital Commit
    Inital Commit

---
+++ hello-resource/src/test/java/com/nosbielc/hello/NativeresourceIT.java
@@ -0,0 +1,9 @@
+package com.nosbielc.hello;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeresourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
